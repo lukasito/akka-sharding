6657eec36c9ed052dd0abd8c1adbab4af6f4605e
Lukáš Klim <lukas.klim@wirecard.com>
2018-12-05 13:19:55 +0100

    small changes.

--- src/gatling/simulations/sk/lukasito/PerformanceTest.scala
+++ src/gatling/simulations/sk/lukasito/PerformanceTest.scala
@@ -8,15 +8,16 @@ import scala.util.Random
 class PerformanceTest extends Simulation {
 
   val scn = scenario("PerformanceTest")
-    .exec {
-      http("process payments")
-        .get(s => {
-          s"http://localhost:9090/merchants/${Math.abs(new Random().nextInt())}/payments/${Math.abs(new Random().nextInt())}"
-        })
-        .check(status.is(200))
+    .repeat(2000, "i") {
+      exec {
+        http("process payments")
+          .get(s => {
+            s"http://localhost:9090/merchants/${Math.abs(new Random().nextInt(10))}/payments/" + Math.abs(new Random().nextInt(10))
+          })
+          .check(status.is(200))
+      }
     }
 
   setUp(
-    scn.inject(constantUsersPerSec(400) during 15)
-  )
+    scn.inject(atOnceUsers(10)))
 }
--- src/main/scala/sk/lukasito/akkasharding/application/Controller.scala
+++ src/main/scala/sk/lukasito/akkasharding/application/Controller.scala
@@ -1,6 +1,7 @@
 package sk.lukasito.akkasharding.application
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.{HttpApp, Route}
 import akka.stream.ActorMaterializer
@@ -10,12 +11,12 @@ object Controller extends HttpApp {
 
   implicit val system = ActorSystem("my-system")
   implicit val materializer = ActorMaterializer()
-  implicit val executionContext = system.dispatcher
-
+  val log = Logging(system, "Controller")
 
   override protected def routes: Route =
     path("merchants" / IntNumber / "payments" / IntNumber) { (maid, pid) =>
       get {
+        log.info("Request: merchants/{}/payments/{}", maid, pid)
         val merchant = Merchant(maid)
         val payment = Payment(pid)
         val processed = PaymentProcessor.processPayment(merchant, payment)
